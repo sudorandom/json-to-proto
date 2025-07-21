018441facbebb0860dcdc41a1e01a551eac1047b
Kevin McDonald <github@sudorandom.com>
2025-07-21 21:12:57 +0200

    declare types

---
+++ src/types/react-syntax-highlighter.d.ts
@@ -0,0 +1,4 @@
+declare module 'react-syntax-highlighter';
+declare module 'react-syntax-highlighter/dist/esm/styles/hljs/atelier-sulphurpool-dark';
+declare module 'react-syntax-highlighter/dist/esm/languages/hljs/json';
+declare module 'react-syntax-highlighter/dist/esm/languages/hljs/protobuf';
